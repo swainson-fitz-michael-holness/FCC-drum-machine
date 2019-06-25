c1f5e9dc8df7b5d621e3880562b35ef76e907810
Swainson Holness <swainson.holness@gmail.com>
2019-06-24 23:38:58 -0600

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Q_wav from './sounds/Q.wav';
+import W_wav from './sounds/W.wav';
+import E_wav from './sounds/E.wav';
+import A_wav from './sounds/A.wav';
+import S_wav from './sounds/S.wav';
+import D_wav from './sounds/D.wav';
+import Z_wav from './sounds/Z.wav';
+import X_wav from './sounds/X.wav';
+import C_wav from './sounds/C.wav';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+var trigger = ["Q","W","E","A","S","D","Z","X","C"]
+var soundBank = [Q_wav,W_wav,E_wav,A_wav,S_wav,D_wav,Z_wav,X_wav,C_wav]
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      test: true
+    }
+  }
+
+  playDrum = (e) => {
+    var drum_ply =document.getElementById(e.target.id.slice(0,1)); 
+    // alert(e.target.id.slice(0,1));
+    drum_ply.pause();
+    drum_ply.currentTime = 0;
+    drum_ply.play();
+    
+  }
+
+  // playKeyDrum = () => {
+  //   alert("boo")
+  // }
+
+  render() {
+    return (
+      <div className="App">
+  
+        <header className="App-header">
+          <div id="drum-machine">
+            <div id="display"></div>
+  
+            {
+              trigger.map((el, index) => {
+                return (<div className="drum-pad" id={el + "_pad"} key={index+"_pad"} onClick={(e) => this.playDrum(e)} onKeyDown={this.playKeyDrum}>
+                  {el}
+                  <audio className="clip" id={el} src={soundBank[index]}></audio>
+                </div>)
+              })
+            }
+          </div>
+         
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
